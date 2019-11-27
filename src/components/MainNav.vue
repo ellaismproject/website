16992fe70a0a855059965475f1177f5b18dd7e03
majordutch <354294+majordutch@users.noreply.github.com>
2019-11-26 20:54:10 -0800

    Update lang

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,27 +1,27 @@
 <template>
-    <b-navbar type="is-dark" fixed-top wrapper-class="container">
+    <b-navbar wrapper-class="container">
         <template slot="brand">
             <b-navbar-item tag="router-link" :to="{ name: 'home' }">
-                <img src="../assets/logo.png" v-bind:alt="$t('navigation.ellaism')"/>
+                <img src="../assets/ellaism-logo-color-name.svg" v-bind:alt="$t('navigation.ellaism')"/>
             </b-navbar-item>
         </template>
+        <template slot="end">
+            <LocaleChanger/>
+        </template>
         <template slot="start">
-            <b-navbar-item tag="router-link" :to="{ name: 'home' }">
-                {{ $t('navigation.ellaism') }}
-            </b-navbar-item>
             <b-navbar-dropdown v-bind:label="$t('navigation.getting_started')">
                 <b-navbar-item tag="div">
                     <strong>{{ $t('navigation.getting_started_subnav.user') }}</strong>
                 </b-navbar-item>
                 <b-navbar-item tag="router-link" :to="{ name: 'wallet' }">
                     <span>
-                        <b-icon pack="fas" icon="wallet"></b-icon>
+                        <b-icon pack="fas" icon="wallet"/>
                         {{ $t('navigation.getting_started_subnav.wallet') }}
                     </span>
                 </b-navbar-item>
                 <b-navbar-item tag="router-link" :to="{ name: 'mining' }">
                     <span>
-                        <b-icon pack="fas" icon="microchip"></b-icon>
+                        <b-icon pack="fas" icon="microchip"/>
                         {{ $t('navigation.getting_started_subnav.mining') }}
                     </span>
                 </b-navbar-item>
@@ -31,100 +31,93 @@
                 </b-navbar-item>
                 <b-navbar-item tag="router-link" :to="{ name: 'testnet' }">
                     <span>
-                        <b-icon pack="fas" icon="server"></b-icon>
+                        <b-icon pack="fas" icon="server"/>
                         {{ $t('navigation.getting_started_subnav.testnet') }}
                     </span>
                 </b-navbar-item>
                 <b-navbar-item tag="router-link" :to="{ name: 'dapp' }">
                     <span>
-                        <b-icon pack="fas" icon="window-restore"></b-icon>
+                        <b-icon pack="fas" icon="window-restore"/>
                         {{ $t('navigation.getting_started_subnav.dapp') }}
                     </span>
                 </b-navbar-item>
                 <b-navbar-item tag="router-link" :to="{ name: 'bot' }">
                     <span>
-                        <b-icon pack="fas" icon="robot"></b-icon>
+                        <b-icon pack="fas" icon="robot"/>
                         {{ $t('navigation.getting_started_subnav.bot') }}
                     </span>
                 </b-navbar-item>
             </b-navbar-dropdown>
-            <b-navbar-item tag="router-link" :to="{ name: 'about' }">
-                {{ $t('navigation.about') }}
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ name: 'donate' }">
-                {{ $t('navigation.donate') }}
-            </b-navbar-item>
-            <b-navbar-dropdown v-bind:label="$t('navigation.community')">
+            <b-navbar-dropdown v-bind:label="$t('navigation.resource')">
                 <b-navbar-item tag="div">
-                    <strong>{{ $t('navigation.community_subnav.official') }}</strong>
+                    <strong>{{ $t('navigation.resource_subnav.official') }}</strong>
                 </b-navbar-item>
                 <b-navbar-item href="https://explorer.ellaism.io">
                     <span>
-                        <b-icon pack="fas" icon="cubes"></b-icon>
-                        {{ $t('navigation.community_subnav.block_explorer') }}
+                        <b-icon pack="fas" icon="cubes"/>
+                        {{ $t('navigation.resource_subnav.block_explorer') }}
                     </span>
                 </b-navbar-item>
                 <b-navbar-item href="https://github.com/ellaismproject" rel="nofollow">
                     <span>
-                        <b-icon pack="fab" icon="github"></b-icon>
-                        {{ $t('navigation.community_subnav.github') }}
+                        <b-icon pack="fab" icon="github"/>
+                        {{ $t('navigation.resource_subnav.github') }}
                     </span>
                 </b-navbar-item>
                 <hr class="dropdown-divider">
                 <b-navbar-item tag="div">
-                    <strong>{{ $t('navigation.community_subnav.community') }}</strong>
+                    <strong>{{ $t('navigation.resource_subnav.community') }}</strong>
                 </b-navbar-item>
                 <b-navbar-item href="https://discordapp.com/invite/gz9tURY" rel="nofollow">
                     <span>
-                        <b-icon pack="fab" icon="discord"></b-icon>
-                        {{ $t('navigation.community_subnav.discord') }}
+                        <b-icon pack="fab" icon="discord"/>
+                        {{ $t('navigation.resource_subnav.discord') }}
                     </span>
                 </b-navbar-item>
                 <b-navbar-item href="https://www.reddit.com/r/ellaism/" rel="nofollow">
                     <span>
-                        <b-icon pack="fab" icon="reddit"></b-icon>
-                        {{ $t('navigation.community_subnav.reddit') }}
+                        <b-icon pack="fab" icon="reddit"/>
+                        {{ $t('navigation.resource_subnav.reddit') }}
                     </span>
                 </b-navbar-item>
                 <b-navbar-item href="https://t.me/ellaismcoin" rel="nofollow">
                     <span>
-                        <b-icon pack="fab" icon="telegram"></b-icon>
-                        {{ $t('navigation.community_subnav.telegram') }}
+                        <b-icon pack="fab" icon="telegram"/>
+                        {{ $t('navigation.resource_subnav.telegram') }}
                     </span>
                 </b-navbar-item>
                 <b-navbar-item href="https://twitter.com/ellacommunity" rel="nofollow">
                     <span>
-                        <b-icon pack="fab" icon="twitter"></b-icon>
-                        {{ $t('navigation.community_subnav.twitter') }}
+                        <b-icon pack="fab" icon="twitter"/>
+                        {{ $t('navigation.resource_subnav.twitter') }}
                     </span>
                 </b-navbar-item>
             </b-navbar-dropdown>
-        </template>
-        <template slot="end">
-            <b-navbar-item href="https://explorer.ellaism.io">
-                <b-icon pack="fas" icon="cubes"></b-icon>
-                {{ $t('navigation.blockchain_explorer') }}
+            <b-navbar-item tag="router-link" :to="{ name: 'donate' }">
+                {{ $t('navigation.donate') }}
             </b-navbar-item>
-            <!--<LocaleChanger/>-->
+            <b-navbar-item tag="router-link" :to="{ name: 'about' }">
+                {{ $t('navigation.about') }}
+            </b-navbar-item>
         </template>
     </b-navbar>
 </template>
 
 <script lang="ts">
-import {Component, Vue, PropSync} from 'vue-property-decorator';
-import LocaleChanger from '@/components/LocaleChanger.vue';
+    import {Component, Vue, PropSync} from 'vue-property-decorator';
+    import LocaleChanger from '@/components/LocaleChanger.vue';
 
-@Component({
-    components: {LocaleChanger},
-})
-export default class MainNav extends Vue {
-}
+    @Component({
+        components: {LocaleChanger},
+    })
+    export default class MainNav extends Vue {
+    }
 </script>
 
-<style scoped>
-    .navbar.is-dark .navbar-brand > a.navbar-item:focus,
-    .navbar.is-dark .navbar-brand > a.navbar-item:hover,
-    .navbar.is-dark .navbar-brand > a.navbar-item.is-active,
+<style>
+    .navbar.is-ella .navbar-brand > a.navbar-item:focus,
+    .navbar.is-ella .navbar-brand > a.navbar-item:hover,
+    .navbar.is-ella .navbar-brand > a.navbar-item.is-active,
     .navbar a.navbar-item.no-highlight:focus,
     .navbar a.navbar-item.no-highlight:hover,
     .navbar a.navbar-item.no-highlight.is-active {
@@ -133,5 +126,10 @@ export default class MainNav extends Vue {
 
     .navbar a.navbar-item .icon {
         margin-right: 0.5rem;
+    }
+
+    .navbar .navbar-start {
+        flex-grow: 1;
+        justify-content: center;
     }
 </style>
