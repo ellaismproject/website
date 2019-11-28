7cea2dbdda8764ca6fb26b649ebab4786ac3bb83
majordutch <354294+majordutch@users.noreply.github.com>
2019-11-27 22:24:39 -0800

    Home page tweaks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <section class="hero is-medium">
+        <section class="hero is-medium is-intro">
             <div class="hero-body">
                 <div class="container has-text-centered">
                     <p class="hero-summary is-size-1-desktop is-size-3">{{ $t('page.home.summary') }}</p>
@@ -15,16 +15,16 @@
                 </div>
             </div>
         </section>
-        <section class="hero is-medium is-ella overview">
+        <section class="hero is-medium is-ella is-overview">
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <i18n path="page.home.pitch.content" tag="p" class="is-size-3-desktop has-text-weight-light" style="margin-bottom:1.75rem;">
+                    <i18n path="page.home.pitch.content" tag="p" class="is-pitch is-size-3-desktop">
                         <abbr slot="evm" v-bind:title="this.$t('page.home.pitch.ethereum_virtual_machine')">{{
                             $t('page.home.pitch.evm') }}</abbr>
                         <abbr slot="wasm" v-bind:title="this.$t('page.home.pitch.webassembly')">{{
                             $t('page.home.pitch.wasm') }}</abbr>
                     </i18n>
-                    <b-button tag="router-link" :to="{ name: 'testnet' }" class="is-size-4-desktop" type="is-dark"
+                    <b-button tag="router-link" :to="{ name: 'testnet' }" class="is-medium" type="is-dark"
                               icon-pack="fas" icon-left="info-circle">
                         {{ $t('page.home.pitch.button') }}
                     </b-button>
@@ -83,20 +83,33 @@
     }
 </script>
 
-<style scoped>
-    .overview {
-        padding-top: 130px;
+<style lang="scss" scoped>
+    @import '~bulma/sass/utilities/initial-variables';
+
+    .is-overview {
+        padding-top: 60px;
         background: url(../assets/home-overview-divider.svg) top no-repeat;
         background-size: cover;
     }
 
-    .hero-summary {
+    @media screen and (min-width: $desktop) {
+        .is-overview {
+            padding-top: 100px;
+        }
+    }
+
+    .is-intro .hero-summary {
         max-width: 750px;
         margin: 0 auto 1.75rem;
         font-weight: lighter;
     }
 
-    .hero .buttons {
+    .is-intro .buttons {
         justify-content: center;
+    }
+
+    .is-pitch {
+        margin-bottom: 1.75rem;
+        font-weight: lighter;
     }
 </style>
