905d9e8ca481ae2fe28670e0a893b6e2061035f4
majordutch <354294+majordutch@users.noreply.github.com>
2019-12-25 17:14:35 -0800

    Update donate page content Add new language content

--- src/views/Donate.vue
+++ src/views/Donate.vue
@@ -4,52 +4,41 @@
         <section class="section">
             <div class="container">
                 <div class="content">
-                    <p>Ellaism did not have an ICO and is a coin without a pre-mine. Donations are welcomed to keep
-                        those that contributed to the coin rewarded. This page was set up so that you can donate to some
-                        people who have contributed code either directly or indirectly to Ellaism.</p>
-                    <h3>Development Fund</h3>
-                    <p><span class="mr"><strong>0xe9C2d958E6234c862b4AfBD75b2fd241E9556303</strong></span>
-                        <a href="https://explorer.ellaism.io/address/0xe9c2d958e6234c862b4afbd75b2fd241e9556303"
-                           class="button is-small"><span>View on Explorer</span>
-                            <b-icon pack="fas" icon="external-link-alt"/>
-                        </a></p>
-                    <p>The Development Fund is a multisig wallet used to support the core development team, operate
-                        infrastructures of Ellaism, and encourage other development activities related to the Ellaism
-                        blockchain. <!--You can find the current signer information, deployment details and instructions for
-                        verifying the proof from this document. Financial reports are published every month for
-                        transparency. You can learn more about the role which Dev Fund and Ellaism Core plays in the
-                        whole Ellaism community from the roadmap.--></p>
-                    <p>Currently the fund is used for:</p>
-                    <ul>
-                        <li>Ellaism blockchain explorer</li>
-                        <li>Ellaism public JSON-RPC API</li>
-                        <li>Ellaism specifications and protocol upgrades</li>
-                        <li>Ellaism Name System (ENS)</li>
-                        <li>Other core development activities</li>
-                    </ul>
-                    <!--<p> Besides making donations, you can also support the Dev Fund by joining the Dev Mining Pool and
-                        many other mining pools. You can find more information in Mining. The transparency page shows
-                        all the outgoing transactions from the Dev Fund.</p>-->
-                    <hr>
-                    <h3>Community Fund</h3>
-                    <p><span class="mr"><strong>0x01ff0FFd25B64dE2217744fd7d4dc4aA3cAbceE7</strong></span> <a
-                            href="https://explorer.ellaism.io/address/0x01ff0ffd25b64de2217744fd7d4dc4aa3cabcee7"
-                            class="button is-small"><span>View on Explorer</span>
-                        <b-icon pack="fas" icon="external-link-alt"/>
-                    </a></p>
-                    <p>The Community Fund is a multisig wallet is managed by community members to support community
-                        activities on the Ellaism blockchain. You can find the current signer information and deployment
-                        details from this document. The transparency page shows all the outgoing transactions from the
-                        Community Fund.</p>
-                    <hr>
-                    <h3>Split Contract</h3>
-                    <p><span class="mr"><strong>0xEDae451f57B5bfF81d1D9eE64F591Ad6a865a652</strong></span> <a
-                            href="https://explorer.ellaism.io/address/0xedae451f57b5bff81d1d9ee64f591ad6a865a652"
-                            class="button is-small"><span>View on Explorer</span>
-                        <b-icon pack="fas" icon="external-link-alt"/>
-                    </a></p>
-                    <p>This contract allows you to split your donation evenly between the Development Fund and Community
-                        Fund.</p>
+                    <p>{{ $t('page.donate.intro') }}</p>
+                    <blockquote>
+                        <p>{{ $t('page.donate.governance_note') }}</p>
+                    </blockquote>
+                    <h2>{{ $t('page.donate.donation_title') }}</h2>
+                    <p>{{ $t('page.donate.donation_intro') }}</p>
+                    <div class="columns">
+                        <div class="column">
+                            <FundWallet hash="0xe9C2d958E6234c862b4AfBD75b2fd241E9556303"
+                                        :title="$t('page.donate.development_fund_title')"
+                                        documentation-url="https://github.com/ellaismproject/ellaism/blob/master/doc/funds.md#development-fund-wallet"
+                                        quorum-min="2" quorum-total="3">
+                                <p>{{ $t('page.donate.development_fund_intro') }}</p>
+                                <ul>
+                                    <li>{{ $t('page.donate.development_fund_point_1') }}</li>
+                                    <li>{{ $t('page.donate.development_fund_point_2') }}</li>
+                                    <li>{{ $t('page.donate.development_fund_point_3') }}</li>
+                                    <li>{{ $t('page.donate.development_fund_point_4') }}</li>
+                                </ul>
+                            </FundWallet>
+                        </div>
+                        <div class="column">
+                            <FundWallet hash="0x01ff0ffd25b64de2217744fd7d4dc4aa3cabcee7"
+                                        :title="$t('page.donate.community_fund_title')"
+                                        documentation-url="https://github.com/ellaismproject/ellaism/blob/master/doc/funds.md#community-fund-wallet"
+                                        quorum-min="6" quorum-total="11">
+                                <p>{{ $t('page.donate.community_fund_intro') }}</p>
+                                <ul>
+                                    <li>{{ $t('page.donate.community_fund_point_1') }}</li>
+                                    <li>{{ $t('page.donate.community_fund_point_2') }}</li>
+                                    <li>{{ $t('page.donate.community_fund_point_3') }}</li>
+                                </ul>
+                            </FundWallet>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
@@ -59,9 +48,10 @@
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
     import PageHeading from '@/components/PageHeading.vue';
+    import FundWallet from '@/components/FundWallet.vue';
 
     @Component({
-        components: {PageHeading},
+        components: {PageHeading, FundWallet},
         metaInfo() {
             return {
                 title: this.$t('page.donate.meta_title').toString(),
@@ -71,9 +61,3 @@
     export default class Donate extends Vue {
     }
 </script>
-
-<style scoped>
-    .mr {
-        margin-right: 0.5rem;
-    }
-</style>
